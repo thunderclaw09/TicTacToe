ac917603b044fb488e1decafbf6eceb6a017c5e7
Elisa <elisalam07@gmail.com>
2022-04-20 21:55:40 -0400

    "initial commit. Right now, can't get the reload to work because it is called while the thing is still being created. It's 10PM right now and I'm tired."

---
+++ TicTacToe.py
@@ -0,0 +1,67 @@
+#Weight on mars is 38% of person's weight on earth.
+from tkinter import *
+from tkinter import ttk
+from Button import Square
+
+
+class TicTacToe:
+    def __init__(self):
+        self.root = Tk()
+        self.frm = ttk.Frame(self.root, padding=40)
+        self.frm.grid()
+
+        Square1 = Square(self.frm, 0, 0)
+
+
+        # self.Answer = ttk.Label(self.frm, text=" ")
+        # self.Answer.grid(column=0, row=5)
+        # self.InputNum = ttk.Entry(self.frm, width=40)
+        # self.InputNum.grid(row=2)
+
+        # self.label1 = ttk.Label(self.frm, text="Please input your a number, and the program").grid(column=0, row=0)
+        # self.label2 = ttk.Label(self.frm, text="will tell you if it's a prime number.").grid(column=0, row=1)
+        
+        # self.Close = ttk.Button(self.frm, text="Quit", command=self.root.destroy).grid(column=3, row=5)
+        # self.Enter = ttk.Button(self.frm, text="Enter", command=self.EnterFunction).grid(column=3, row=2)
+
+        
+
+        mainloop()
+
+
+    # def EnterFunction(self):
+    #     print("Enter pressed!")
+    #     Number = int(self.InputNum.get())
+    #     divisor = 2
+    #     Output = ""
+    #     while True:
+    #         result = Number % divisor
+    #         if result > 0:
+    #             divisor+=1
+    #         elif Number == 2:
+    #             Output = "The number is a prime number!"
+    #             break
+    #         elif result == 0:
+    #             Output = "The number is not a prime number!"
+    #             break
+    #         elif divisor == Number:
+    #             Output = "The number is a prime number!"
+    #             break
+            
+
+        # self.Answer.config(text=Output) 
+        
+        print("It should have worked.")
+
+
+    
+    
+
+    
+
+  
+
+ticTacToe = TicTacToe()
+
+
+
